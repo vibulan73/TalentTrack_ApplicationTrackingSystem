4af201793be16bf366d9b2aef34a1f79669b422a
vibulan73 <vibulan2000@gmail.com>
2025-12-26 19:01:17 +0530

    Remove unused useNavigate import

--- ats-frontend/src/pages/ApplicationForm.js
+++ ats-frontend/src/pages/ApplicationForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate, Link } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import api from '../api/axios';
 
 const ApplicationForm = () => {
